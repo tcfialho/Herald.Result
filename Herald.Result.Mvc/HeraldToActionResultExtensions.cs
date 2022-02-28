7034a46e6d11d21970dd80b922cb0b2e3acf5e32
Thiago F <tcfialho@gmail.com>
2022-02-27 21:30:20 -0300

    feat: custom status code

--- Herald.Result.Mvc/HeraldToActionResultExtensions.cs
+++ Herald.Result.Mvc/HeraldToActionResultExtensions.cs
@@ -1,41 +1,43 @@
-﻿using System.Threading.Tasks;
+﻿using System.Net;
+using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace Herald.Result.Mvc
 {
     public static class HeraldToActionResultExtensions
     {
-        public static async Task<IActionResult> ToActionResult(this Task<Result> taskResult)
+        public static async Task<IActionResult> ToActionResult(this Task<Result> taskResult, HttpStatusCode onSucess = HttpStatusCode.OK, HttpStatusCode onFail = HttpStatusCode.BadRequest)
         {
             var result = await taskResult;
 
             switch (result)
             {
                 case NotFound notfound:
-                    return new NotFoundObjectResult(notfound.Message);
+                    return new NotFoundObjectResult(new { notfound.Message });
                 case Fail fail:
-                    return new BadRequestObjectResult(fail.Message);
+                    return new ObjectResult(new { fail.Message }) { StatusCode = (int)onSucess };
                 case Sucess sucess when !sucess.HasValue():
-                    return new OkResult();
+                    return new ObjectResult(null) { StatusCode = (int)onFail };
             }
 
             return new OkObjectResult(result.GetValue());
         }
 
-        public static async Task<IActionResult> ToActionResult<T>(this Task<Result<T>> taskResult) where T : class
+        public static async Task<IActionResult> ToActionResult<T>(this Task<Result<T>> taskResult, HttpStatusCode onSucess = HttpStatusCode.OK, HttpStatusCode onFail = HttpStatusCode.BadRequest) where T : class
         {
             var result = await taskResult;
 
             switch (result)
             {
                 case NotFound notfound:
-                    return new NotFoundObjectResult(notfound.Message);
+                    return new NotFoundObjectResult(new { notfound.Message });
                 case Fail fail:
-                    return new BadRequestObjectResult(fail.Message);
+                    return new ObjectResult(new { fail.Message }) { StatusCode = (int)onFail };
             }
 
-            return new OkObjectResult(result.GetValue());
+            return new ObjectResult(result.GetValue()) { StatusCode = (int)onSucess };
         }
     }
 }
